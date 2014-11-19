54e3b374b299dd2c4a090952cca2c6ded774cc86
asell22 <alexanderjsell@gmail.com>
2014-11-18 16:06:04 -0800

    finished assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
+  root to: "pages#index", as: :index
+
+  get 'verse-2' => "pages#verse_2"
+
+  get 'verse-3' => "pages#verse_3"
+
+  get 'verse-4' => "pages#verse_4"
 end
